5de62e026f59be06648aee31f354cc30fdf95da9
c0razon-de-lavadora <danaramgarcia@gmail.com>
2025-06-05 14:03:48 -0600

    actualizarborrar

--- ProyectoMotaFinal/ActBorrar.Designer.cs
+++ ProyectoMotaFinal/ActBorrar.Designer.cs
@@ -28,12 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ActBorrar";
+            btnActualizar = new Button();
+            txtStock = new TextBox();
+            txtPrecio = new TextBox();
+            txtMarca = new TextBox();
+            txtTipo = new TextBox();
+            txtNombre = new TextBox();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            lbl1 = new Label();
+            btnBorrar = new Button();
+            SuspendLayout();
+            // 
+            // btnActualizar
+            // 
+            btnActualizar.Location = new Point(503, 309);
+            btnActualizar.Margin = new Padding(3, 4, 3, 4);
+            btnActualizar.Name = "btnActualizar";
+            btnActualizar.Size = new Size(86, 31);
+            btnActualizar.TabIndex = 21;
+            btnActualizar.Text = "Actualizar";
+            btnActualizar.UseVisualStyleBackColor = true;
+            // 
+            // txtStock
+            // 
+            txtStock.Location = new Point(199, 197);
+            txtStock.Margin = new Padding(3, 4, 3, 4);
+            txtStock.Name = "txtStock";
+            txtStock.Size = new Size(114, 27);
+            txtStock.TabIndex = 20;
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Location = new Point(26, 197);
+            txtPrecio.Margin = new Padding(3, 4, 3, 4);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(114, 27);
+            txtPrecio.TabIndex = 19;
+            // 
+            // txtMarca
+            // 
+            txtMarca.Location = new Point(386, 72);
+            txtMarca.Margin = new Padding(3, 4, 3, 4);
+            txtMarca.Name = "txtMarca";
+            txtMarca.Size = new Size(114, 27);
+            txtMarca.TabIndex = 18;
+            // 
+            // txtTipo
+            // 
+            txtTipo.Location = new Point(204, 72);
+            txtTipo.Margin = new Padding(3, 4, 3, 4);
+            txtTipo.Name = "txtTipo";
+            txtTipo.Size = new Size(114, 27);
+            txtTipo.TabIndex = 17;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(26, 72);
+            txtNombre.Margin = new Padding(3, 4, 3, 4);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(114, 27);
+            txtNombre.TabIndex = 16;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(199, 161);
+            label5.Name = "label5";
+            label5.Size = new Size(45, 20);
+            label5.TabIndex = 15;
+            label5.Text = "Stock";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(26, 161);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 20);
+            label4.TabIndex = 14;
+            label4.Text = "Precio";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(386, 36);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 13;
+            label3.Text = "Marca";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(204, 36);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 20);
+            label2.TabIndex = 12;
+            label2.Text = "Tipo";
+            // 
+            // lbl1
+            // 
+            lbl1.AutoSize = true;
+            lbl1.Location = new Point(26, 36);
+            lbl1.Name = "lbl1";
+            lbl1.Size = new Size(64, 20);
+            lbl1.TabIndex = 11;
+            lbl1.Text = "Nombre";
+            // 
+            // btnBorrar
+            // 
+            btnBorrar.Location = new Point(366, 311);
+            btnBorrar.Name = "btnBorrar";
+            btnBorrar.Size = new Size(94, 29);
+            btnBorrar.TabIndex = 22;
+            btnBorrar.Text = "Borrar";
+            btnBorrar.UseVisualStyleBackColor = true;
+            // 
+            // ActBorrar
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(610, 359);
+            Controls.Add(btnBorrar);
+            Controls.Add(btnActualizar);
+            Controls.Add(txtStock);
+            Controls.Add(txtPrecio);
+            Controls.Add(txtMarca);
+            Controls.Add(txtTipo);
+            Controls.Add(txtNombre);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(lbl1);
+            Name = "ActBorrar";
+            Text = "ActBorrar";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnActualizar;
+        private TextBox txtStock;
+        private TextBox txtPrecio;
+        private TextBox txtMarca;
+        private TextBox txtTipo;
+        private TextBox txtNombre;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label lbl1;
+        private Button btnBorrar;
     }
 }
